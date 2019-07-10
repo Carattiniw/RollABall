1d600f3162bfb62c9c3f72c4e623d841f860afa6
Carattiniw <45584185+Suddenrebel@users.noreply.github.com>
2019-07-09 23:10:06 -0400

    Fixed Escape and Restart Functions

---
+++ Assets/Scenes/Scripts/BackUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackUp : MonoBehaviour
+{
+    /*
+    This was my solution to fixing the escape and restart behaviors
+    if the player is in game over state.
+    */
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
